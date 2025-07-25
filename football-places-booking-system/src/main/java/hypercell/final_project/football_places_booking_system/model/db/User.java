1a367936e16ece4201487020f14df513a26f6651
Abdallahs-Code <abdallahahmed.wrk@gmail.com>
2025-07-25 15:38:12 +0300

    added some features
    - added user CRUD operations
    - added exceptions and error messages
    - integrated security

--- football-places-booking-system/src/main/java/hypercell/final_project/football_places_booking_system/model/db/User.java
+++ football-places-booking-system/src/main/java/hypercell/final_project/football_places_booking_system/model/db/User.java
@@ -1,5 +1,12 @@
 package hypercell.final_project.football_places_booking_system.model.db;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import hypercell.final_project.football_places_booking_system.model.enums.UserRole;
 import hypercell.final_project.football_places_booking_system.model.enums.UserStatus;
 import jakarta.persistence.Column;
@@ -9,21 +16,22 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class User extends BaseEntity {
+@Table(name = "users")
+public class User extends BaseEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String username;
 
     @Column(unique = true)
@@ -36,4 +44,43 @@ public class User extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private UserStatus status;
+
+    public String getUserName() {
+        return this.username;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
 }
--- football-places-booking-system/src/main/java/hypercell/final_project/football_places_booking_system/service/UserService.java
+++ football-places-booking-system/src/main/java/hypercell/final_project/football_places_booking_system/service/UserService.java
@@ -1,4 +1,144 @@
 package hypercell.final_project.football_places_booking_system.service;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import hypercell.final_project.football_places_booking_system.exception.AlreadyExistsException;
+import hypercell.final_project.football_places_booking_system.exception.AppException;
+import hypercell.final_project.football_places_booking_system.exception.NoContentException;
+import hypercell.final_project.football_places_booking_system.exception.NoDataException;
+import hypercell.final_project.football_places_booking_system.exception.NotFoundException;
+import hypercell.final_project.football_places_booking_system.exception.ValidationException;
+import hypercell.final_project.football_places_booking_system.model.db.User;
+import hypercell.final_project.football_places_booking_system.model.dto.ResponseDTO;
+import hypercell.final_project.football_places_booking_system.model.dto.UserDTO;
+import hypercell.final_project.football_places_booking_system.model.enums.ErrorCode;
+import hypercell.final_project.football_places_booking_system.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void register(UserDTO userDTO) throws AppException {
+        if (userDTO.username() == null || userDTO.username().isEmpty()) {
+            throw new ValidationException(ErrorCode.INVALID_USERNAME);
+        }
+
+        if (userRepository.existsByUsername(userDTO.username())) {
+            throw new AlreadyExistsException(ErrorCode.USERNAME_ALREADY_EXISTS);
+        }
+
+        if (userDTO.email() == null || userDTO.email().isEmpty()) {
+            throw new ValidationException(ErrorCode.INVALID_EMAIL);
+        }
+
+        if (userRepository.existsByEmail(userDTO.email())) {
+            throw new AlreadyExistsException(ErrorCode.EMAIL_ALREADY_EXISTS);
+        }
+
+        if (userDTO.password() == null || userDTO.password().isEmpty()) {
+            throw new ValidationException(ErrorCode.INVALID_PASSWORD);
+        }
+
+        if (userDTO.role() == null) {
+            throw new ValidationException(ErrorCode.INVALID_USER_ROLE);
+        }
+
+        if (userDTO.status() == null) {
+            throw new ValidationException(ErrorCode.INVALID_USER_STATUS);
+        }
+
+        User user = new User();
+        user.setUsername(userDTO.username());
+        user.setEmail(userDTO.email());
+        user.setPassword(passwordEncoder.encode(userDTO.password()));
+        user.setRole(userDTO.role());
+        user.setStatus(userDTO.status());
+
+        userRepository.save(user);
+    }
+
+    public UserDTO getUserById(Long id) throws AppException {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            throw new NotFoundException(ErrorCode.USER_NOT_FOUND);
+        }
+        
+        return new UserDTO(
+            user.getId(),
+            user.getUserName(),
+            user.getEmail(),
+            null,
+            user.getRole(),
+            user.getStatus()
+        );
+    }
+
+    public List<UserDTO> getAllUsers() throws AppException {
+        List<User> users = userRepository.findAll();
+        if (users.isEmpty()) {
+            throw new NoContentException(ErrorCode.NO_CONTENT);
+        }
+
+        return users.stream()
+            .map(user -> new UserDTO(
+                user.getId(),
+                user.getUserName(),
+                user.getEmail(),
+                null,
+                user.getRole(),
+                user.getStatus()
+            ))
+            .toList();
+    }
+
+    public ResponseEntity<ResponseDTO> updateUser(Long id, UserDTO userDTO) throws AppException {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            throw new NotFoundException(ErrorCode.USER_NOT_FOUND);
+        }
+
+        if (userDTO.username() == null &&
+            userDTO.password() == null && userDTO.role() == null && userDTO.status() == null) {
+            throw new NoDataException(ErrorCode.NO_DATA);
+        }
+
+        if (userDTO.username() != null && !userDTO.username().isEmpty()) {
+            if (userRepository.existsByUsername(userDTO.username())) {
+                throw new AlreadyExistsException(ErrorCode.USERNAME_ALREADY_EXISTS);
+            }
+            user.setUsername(userDTO.username());
+        }
+
+        if (userDTO.password() != null && !userDTO.password().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(userDTO.password()));
+        }
+
+        if (userDTO.role() != null) {
+            user.setRole(userDTO.role());
+        }
+
+        if (userDTO.status() != null) {
+            user.setStatus(userDTO.status());
+        }
+
+        userRepository.save(user);
+        return ResponseEntity.ok(new ResponseDTO(id, "User updated successfully"));
+    }
+
+    public ResponseEntity<ResponseDTO> deleteUser(Long id) throws AppException {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            throw new NotFoundException(ErrorCode.USER_NOT_FOUND);
+        }
+        userRepository.delete(user);
+        
+        return ResponseEntity.ok(new ResponseDTO(id, "User deleted successfully"));
+    }
 }
