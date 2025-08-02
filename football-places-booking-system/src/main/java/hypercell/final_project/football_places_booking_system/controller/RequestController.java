9c9b3a475b25c93f6cf92a4fbbd7b5a7b70f1e53
Omar-Saad-ELGharbawy <84602951+Omar-Saad-ELGharbawy@users.noreply.github.com>
2025-08-02 20:46:32 +0300

    add get request with id from token

--- football-places-booking-system/src/main/java/hypercell/final_project/football_places_booking_system/controller/RequestController.java
+++ football-places-booking-system/src/main/java/hypercell/final_project/football_places_booking_system/controller/RequestController.java
@@ -3,12 +3,15 @@ package hypercell.final_project.football_places_booking_system.controller;
 import java.util.List;
 import java.util.UUID;
 
+import hypercell.final_project.football_places_booking_system.model.db.User;
 import lombok.RequiredArgsConstructor;
 
 import hypercell.final_project.football_places_booking_system.model.db.Request;
 import hypercell.final_project.football_places_booking_system.exception.AppException;
 import hypercell.final_project.football_places_booking_system.service.Interfaces.RequestService;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,5 +26,13 @@ public class RequestController {
     @GetMapping("/received/{receiverId}")
     public List<Request> getReceivedRequests(@PathVariable UUID receiverId) throws AppException {
         return requestService.getRequestsByReceiver(receiverId);
+    }
+
+    @GetMapping("/received")
+    public List<Request> getReceivedRequests(@AuthenticationPrincipal UserDetails userDetails) throws AppException {
+        User user = (User) userDetails;
+//        receiverId = user.getId()
+
+        return requestService.getRequestsByReceiver(user.getId());
     }
 }
